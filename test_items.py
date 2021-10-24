6ba8490f00f2793a7a2775e22c70823db671078f
Ilya Filimonov <ilifilimonov@ozon.ru>
2021-10-24 23:10:03 +0300

    unnecessary imports were cleaned

--- test_items.py
+++ test_items.py
@@ -1,17 +1,9 @@
-import selenium.common.exceptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_check_add_to_basket_is_available(browser):
     browser.get(link)
-
-    add_to_basket_button = None
-    try:
-        add_to_basket_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-    except selenium.common.exceptions.NoSuchElementException:
-        add_to_basket_button = False
-
-    assert add_to_basket_button, 'There is no "add to basket" button available!'
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket").is_displayed(),\
+        'There is no "add to basket" button available!'
