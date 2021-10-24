fcbd1f7baf5f86554ceb0a949dd81df2aa52e011
Hoventage <49985493+Hoventage@users.noreply.github.com>
2021-10-24 22:17:08 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_check_add_to_basket_is_available(browser):
     browser.get(link)
-    driver_wait = WebDriverWait(browser, 10)
 
     add_to_basket_button = None
     try:
